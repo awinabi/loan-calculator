1aab444d38fd3b72dcbfa0ecee376eb4dd8bcdf2
Awin Abi <awinabi@gmail.com>
2021-11-22 23:41:48 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { css, jsx } from '@emotion/react'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import styled from '@emotion/styled'
+
+import logo from './logo.svg';
+
+const marks = [
+  {
+    value: 5,
+    label: '5L',
+  },
+  {
+    value: 25,
+    label: '25L',
+  },
+  {
+    value: 50,
+    label: '50L',
+  },
+  {
+    value: 75,
+    label: '75L',
+  },
+  {
+    value: 100,
+    label: '1Cr',
+  },
+
+  {
+    value: 150,
+    label: '1.50Cr',
+  },
+];
+
+function valuetext(value) {
+  return `${value}L`;
 }
 
-export default App;
+export default function App() {
+  return (
+
+    <Box>
+      <Card sx={{ minWidth: 275, margin: "40px" }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Loan Amount
+        </Typography>
+        <Slider
+          aria-label="Loan Amount"
+          defaultValue={25}
+          getAriaValueText={valuetext}
+          step={25}
+          max={200}
+          valueLabelDisplay="auto"
+          marks={marks}
+        />
+        <Typography sx={{ fontSize: 14, marginTop: "30px" }} color="text.secondary" gutterBottom>
+          Tenure
+        </Typography>
+        <Slider
+          aria-label="Tenure"
+          defaultValue={25}
+          getAriaValueText={valuetext}
+          step={5}
+          max={30}
+          marks={[5,10,15,20,25,30].map((val)=>({"value": val, "label": `${val}`}))}
+          valueLabelDisplay="auto"
+        />
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" size="small">Calculate</Button>
+      </CardActions>
+    </Card>
+
+    </Box>
+  );
+}
